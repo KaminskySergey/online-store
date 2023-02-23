b404591d44acab1761c87df39f352b9f78674efc
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2023-02-24 00:44:56 +0200

    API

---
+++ src/API/API.jsx
@@ -0,0 +1,22 @@
+// ======================== PRODUCTS
+
+const Product = {
+    id: 1,
+    name: "Blender ROWENTA X251",
+    category: "small appliances",
+    price: 1250,
+    description: "Super blender for every day use",
+    image: "http://someImage.jpg"
+}
+
+// ======================== USER
+
+const User = {
+    id: 1,
+    admin: false,
+    firstName: "John",
+    lastName: "Silver",
+    email: "john.silver@gmail.com",
+    phone: "+380500000000",
+    password: "qwe123",
+}
